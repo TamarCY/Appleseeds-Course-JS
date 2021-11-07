5b671b7e7a5ef19533486cde4b9f18e2dd3b05a8
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-07 13:22:14 +0200

    1.2

---
+++ 1.2.js
@@ -0,0 +1,73 @@
+/*
+--Delete the wrong answers--
+
+1. Which of the following is/are strings?
+
+
+c) '4'
+
+2. Which of the following is/are numbers?
+
+a) 4
+b) 4.0
+d) -4
+
+3. Which of the following is/are booleans?
+
+a) true
+b) false
+
+
+4. What is the result of 80 + 71.2?
+
+
+
+c) 8071.2
+
+5. What is the result of "80" + 71.2?
+
+
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+
+b)string
+
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+
+let my_var;
+let my_var=100;
+my_var=100;
+
+//declare a variable and reassign a value to it
+let new_var;
+new_var = "I am new";
+
+//create a variable and after that assign a string with its value being: He's got it!
+let my_string;
+my_string = "He's got it";
+/*
+1. create a variable and assign a value on how much a restaurant bill is.
+2. create a variable and assign a value on how much tax they should pay.
+3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+ */
+
+let bill = 100;
+let tax = 17;
+
+let bill_after_tax = "Your total bill is "+  ((bill*tax/100)+bill) + "$"; 
+console.log (bill_after_tax);
+
+
+// Round the number 50.6 to its nearest integer
+Math.round(5)
+
+//Create a variable that is undefined
