ffb1721390fe1381a2ad82c7678c63ea2b2d50e8
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-07 14:51:12 +0200

    2.3

--- 2.3.js
+++ 2.3.js
@@ -1,7 +1,5 @@
-let circelArea=(radius)=>(Math.round(Math.PI*radius*radius))
-circelArea(4);
 
-// why this is not working?
+
 
 let circelArea2=(radius)=>(Math.round(Math.PI*Math.pow(radius,2)))
 circelArea2(4);
