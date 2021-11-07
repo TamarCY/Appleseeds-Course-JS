5f7531d1de075eb51b3d011e98ec71f00ae8163e
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-07 13:17:05 +0200

    typeof

--- 1.1.js
+++ 1.1.js
@@ -1,18 +1,30 @@
 let my_string = "I am a string";
 console.log("my string :"+ my_string);
+console.log(typeof my_string);
 let my_num = 100;
 console.log("my num:" + my_num);
+console.log(typeof my_num);
 let my_object = new Object();
 my_object.age = 37;
+console.log(typeof my_object);
 console.log ("my object is:"  + my_object);
 console.log ("my age is:"  + my_object.age);
 
 let my_boolean = true;
 console.log ("my boolean is:"  + my_boolean);
+console.log(typeof my_boolean);
+
 let my_null = null;
 console.log ("my null is:" + my_null);
+console.log(typeof my_null);
+
 let my_undefiend = undefined;
 console.log ("my undefined is:" + my_undefiend);
+console.log(typeof my_undefiend);
+
 let my_symbol = Symbol('I am a symbol');
 console.log(my_symbol);
 console.log (typeof my_symbol);
+
+console.log("coooool")
+
