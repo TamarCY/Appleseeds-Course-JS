04c0b7bd29172f9ad7f1be4f3d71dac0ef8f8052
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-07 15:57:48 +0200

    2.4

---
+++ 2.4.js
@@ -0,0 +1,15 @@
+const days=["Sunday", "Monday", "Tusday", "Wednsday","Thursday","Friday","Saterday"];
+const month =["January", "February", "March", "April", "May", "June", "July", "August" ,"September" ,"October" ,"November" ,"December"]
+let ToDay=()=>{
+    let myDate = new Date();
+    let day = days[myDate.getDay()];
+    let monthDay = myDate.getDate();
+    let year = myDate.getFullYear();
+    let myMonth = month[myDate.getMonth()];
+    
+    let answ="Today is " + day + " the " + monthDay + " of " + myMonth + ", " + year;
+    return answ;
+}
+ToDay();
+
+
