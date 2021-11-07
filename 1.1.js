08186fa4f6b1db8948d88e37d668f42df35078be
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-07 12:01:01 +0200

    1.1

---
+++ 1.1.js
@@ -0,0 +1,18 @@
+let my_string = "I am a string";
+console.log("my string :"+ my_string);
+let my_num = 100;
+console.log("my num:" + my_num);
+let my_object = new Object();
+my_object.age = 37;
+console.log ("my object is:"  + my_object);
+console.log ("my age is:"  + my_object.age);
+
+let my_boolean = true;
+console.log ("my boolean is:"  + my_boolean);
+let my_null = null;
+console.log ("my null is:" + my_null);
+let my_undefiend = undefined;
+console.log ("my undefined is:" + my_undefiend);
+let my_symbol = Symbol('I am a symbol');
+console.log(my_symbol);
+console.log (typeof my_symbol);
