6cc4be8f07ac8a46152622b1076061dbf4963b3c
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-08 13:24:44 +0200

    4.1

---
+++ 4.1.js
@@ -0,0 +1,17 @@
+let numSiblings = prompt("How many siblings do you have?")
+if (numSiblings == 1){
+    console.log("1 sibling!")
+}
+else{
+    if(numSiblings>1){
+        console.log("More then 1 sibling")
+    }
+    else(
+        console.log("No-siblings")
+    )
+}
+
+
+
+
+
