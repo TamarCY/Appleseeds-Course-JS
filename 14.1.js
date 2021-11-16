03ccb11704c2bbbfbb421661f71dc08b89b28b62
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-16 14:38:53 +0200

    14.1

---
+++ 14.1.js
@@ -0,0 +1,28 @@
+const arr = [1, 2, 3, 4, 5, 6]
+
+let result = arr.reduce((max, n) => {
+    return Math.max(max, n)
+})
+
+
+result = arr.reduce((max, n) => {
+    return max > n ? max : n
+})
+
+// console.log (result)
+
+
+let sumOfEven = arr.reduce((sum, n) =>
+    n % 2 === 0 ?
+        sum + n :
+        sum
+
+    , 0)
+
+
+
+console.log(avg)
+
+let avg2 = arr.reduce((res, n) => (res + n)) / arr.length
+
+console.log(avg2)
