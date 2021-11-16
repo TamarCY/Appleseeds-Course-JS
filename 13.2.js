e4e97bc49fde75c6a61b7000971913beca0edfca
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-11-16 11:36:23 +0200

    13.2

---
+++ 13.2.js
@@ -0,0 +1,17 @@
+const foods = ["falafel", "sabich", "hummus", "pizza with extra",
+"pineapple"];
+
+let sorted = foods.slice().sort();
+
+
+let desndingSorted = foods.slice().sort().reverse();
+
+console.log(sorted);
+
+console.log(desndingSorted);
+
+console.log(foods);
+
+
+
+
