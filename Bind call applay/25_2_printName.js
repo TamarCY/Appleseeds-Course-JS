1ff93d1450b6c145c6c65f9df375cdef89aaac47
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-12-06 14:33:25 +0200

    25.2

---
+++ Bind call applay/25_2_printName.js
@@ -0,0 +1,21 @@
+// Create an object with a name property. The object should
+// also have a method that prints its name, and another
+// method that prints its name after a second with the help of
+// setTimeOut. in this exercise, you are not allowed to use
+// arrow functions.
+
+const myObject = {
+    name: "tamar",
+    printName: function () {
+        console.log(this.name)
+    },
+    printNameTimeOut (){
+        // setTimeout(this.printName,2000)
+
+        setTimeout(this.printName.bind(this),2000)
+    }
+}
+
+
+
+myObject.printNameTimeOut();
